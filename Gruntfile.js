04cc8df30affa63352ceb4df41a589c9a9ae9f55
YOUR NAME <erinpagemd@yahoo.com>
2015-02-23 15:50:18 -0600

    working on Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,23 +2,40 @@
 
 module.exports = function(grunt) {
   
-  // Load the plugin that provides the "copy" task.
-  grunt.loadNpmTasks('grunt-contrib-copy');
-
+  // Load the plugin that provides the task.
+  require('load-grunt-tasks')(grunt);
 
   // Project configuration.
   grunt.initConfig({
+    clean: ['public'],//end clean
     copy: {
       main: {
         files: [
-          {expand: true, cwd: 'app/', src: ['**'], dest: 'public/', filter: 'isFile'}
+          {expand: true, cwd: 'app/', src: ['**', '!**/*.jade', '!**/*.{sass,scss}'], dest: 'public/', filter: 'isFile'}
         ]
       }
-    }
-  });
-
+    },//end copy
+    jade: {
+      compile: {
+        files: [
+          {expand: true, cwd: 'app/', src: ['**/*.jade', '!**/_*.jade'], dest: 'public/', ext: '.html'}
+        ]
+      }
+    },// end jade
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+            'public/css/main.css': 'app/css/main.scss'
+        }
+      }
+    }//end sass
+  });//end initConfig
 
   // Default task(s).
   grunt.registerTask('default', []);
+  grunt.registerTask('build', ['clean', 'copy', 'jade', 'sass']);
 
-};
+};//end module.exports
